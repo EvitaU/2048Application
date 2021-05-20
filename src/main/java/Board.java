07c549fc54be16e8e0540ece15f97456b89227f6
Evita <evita.udre@gmail.com>
2021-05-21 02:24:39 +0300

    Board class. resolves #3

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -5,52 +5,52 @@ public class Board {
     public int score = 0;
 
     public Board() {
-        this.board = new Tile[4][4];
+        board = new Tile[4][4];
 
-        for (int i = 0; i < this.board.length; ++i) {
-            for (int j = 0; j < this.board[i].length; ++j) {
-                this.board[i][j] = new Tile();
+        for (int i = 0; i < board.length; ++i) {
+            for (int j = 0; j < board[i].length; ++j) {
+                board[i][j] = new Tile();
             }
         }
     }
 
     public Board(int grids) {
-        this.grids = grids;
-        this.board = new Tile[grids][grids];
+        grids = grids;
+        board = new Tile[grids][grids];
 
-        for (int i = 0; i < this.board.length; ++i) {
-            for (int j = 0; j < this.board[i].length; ++j) {
-                this.board[i][j] = new Tile();
+        for (int i = 0; i < board.length; ++i) {
+            for (int j = 0; j < board[i].length; ++j) {
+                board[i][j] = new Tile();
             }
         }
     }
 
     public Board(int lose, int grids) {
-        this.grids = grids;
-        this.board = new Tile[grids][grids];
+        grids = grids;
+        board = new Tile[grids][grids];
 
-        for (int i = 0; i < this.board.length; ++i) {
-            for (int j = 0; j < this.board[i].length; ++j) {
-                this.board[i][j] = new Tile((lose + i + j) * (i + j));
+        for (int i = 0; i < board.length; ++i) {
+            for (int j = 0; j < board[i].length; ++j) {
+                board[i][j] = new Tile((lose + i + j) * (i + j));
             }
         }
     }
 
     public Tile[][] getBoard() {
-        return this.board;
+        return board;
     }
 
     public int getScore() {
-        return this.score;
+        return score;
     }
 
     public int getHighTile() {
-        int high = this.board[0][0].getValue();
+        int high = board[0][0].getValue();
 
-        for (int i = 0; i < this.board.length; ++i) {
-            for (int j = 0; j < this.board[i].length; ++j) {
-                if (this.board[i][j].getValue() > high) {
-                    high = this.board[i][j].getValue();
+        for (int i = 0; i < board.length; ++i) {
+            for (int j = 0; j < board[i].length; ++j) {
+                if (board[i][j].getValue() > high) {
+                    high = board[i][j].getValue();
                 }
             }
         }
@@ -58,22 +58,22 @@ public class Board {
     }
 
     public void print() {
-        for (int i = 0; i < this.board.length; ++i) {
-            for (int j = 0; j < this.board[i].length; ++j) {
-                String s = this.board[i][j].toString() + " ";
+        for (int i = 0; i < board.length; ++i) {
+            for (int j = 0; j < board[i].length; ++j) {
+                String s = board[i][j].toString() + " ";
                 System.out.println(s);
             }
             System.out.println("");
         }
-        System.out.println("Score: " + this.score);
+        System.out.println("Score: " + score);
     }
 
     public String toString() {
         String s = "";
 
-        for (int i = 0; i < this.board.length; ++i) {
-            for (int j = 0; j < this.board[i].length; ++j) {
-                s = s + this.board[i][j].toString() + " ";
+        for (int i = 0; i < board.length; ++i) {
+            for (int j = 0; j < board[i].length; ++j) {
+                s = s + board[i][j].toString() + " ";
             }
             s = s + "\n";
         }
@@ -87,12 +87,12 @@ public class Board {
             int row = (int) (Math.random() * 4.0D);
             int col = (int) (Math.random() * 4.0D);
             double x = Math.random();
-            if (this.board[row][col].getValue() == 0) {
-                if (x < 0.2D) {
-                    this.board[row][col] = new Tile(4);
+            if (board[row][col].getValue() == 0) {
+                if (x < 0.2) {
+                    board[row][col] = new Tile(4);
                     empty = false;
                 } else {
-                    this.board[row][col] = new Tile(2);
+                    board[row][col] = new Tile(2);
                     empty = false;
                 }
             }
@@ -101,161 +101,189 @@ public class Board {
 
     public boolean blackOut() {
         int count = 0;
-        for (int i = 0; i < this.board.length; ++i) {
-            for (int j = 0; j < this.board[i].length; ++j) {
-                if (this.board[i][j].getValue() > 0) {
+        for (int i = 0; i < board.length; ++i) {
+            for (int j = 0; j < board[i].length; ++j) {
+                if (board[i][j].getValue() > 0) {
                     ++count;
                 }
             }
         }
         if (count == 16) {
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 
-    public boolean gameOver() {
-        int count = 0;
 
-        for (int i = 0; i < this.board.length; ++i) {
-            for (int j = 0; j < this.board[i].length; ++j) {
-                if (this.board[i][j].getValue() > 0) {
+    public boolean gameOver() {
+
+        int count = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j].getValue() > 0) {
                     if (i == 0 && j == 0) {
-                        if (this.board[i][j].getValue() != this.board[i + 1][j].getValue() && this.board[i][j].getValue() != this.board[i][j + 1].getValue()) {
-                            ++count;
+                        if (board[i][j].getValue() != board[i + 1][j].getValue()
+                                && board[i][j].getValue() != board[i][j + 1].getValue()) {
+                            count++;
                         }
                     } else if (i == 0 && j == 3) {
-                        if (this.board[i][j].getValue() != this.board[i + 1][j].getValue() && this.board[i][j].getValue() != this.board[i][j - 1].getValue()) {
-                            ++count;
+                        if (board[i][j].getValue() != board[i + 1][j].getValue()
+                                && board[i][j].getValue() != board[i][j - 1].getValue()) {
+                            count++;
                         }
                     } else if (i == 3 && j == 3) {
-                        if (this.board[i][j].getValue() != this.board[i - 1][j].getValue() && this.board[i][j].getValue() != this.board[i][j - 1].getValue()) {
-                            ++count;
+                        if (board[i][j].getValue() != board[i - 1][j].getValue()
+                                && board[i][j].getValue() != board[i][j - 1].getValue()) {
+                            count++;
                         }
                     } else if (i == 3 && j == 0) {
-                        if (this.board[i][j].getValue() != this.board[i - 1][j].getValue() && this.board[i][j].getValue() != this.board[i][j + 1].getValue()) {
-                            ++count;
+                        if (board[i][j].getValue() != board[i - 1][j].getValue()
+                                && board[i][j].getValue() != board[i][j + 1].getValue()) {
+                            count++;
                         }
                     } else if (i == 0 && (j == 1 || j == 2)) {
-                        if (this.board[i][j].getValue() != this.board[i + 1][j].getValue() && this.board[i][j].getValue() != this.board[i][j + 1].getValue() && this.board[i][j].getValue() != this.board[i][j - 1].getValue()) {
-                            ++count;
+                        if (board[i][j].getValue() != board[i + 1][j].getValue()
+                                && board[i][j].getValue() != board[i][j + 1].getValue()
+                                && board[i][j].getValue() != board[i][j - 1].getValue()) {
+                            count++;
                         }
-                    } else if (i != 3 || j != 1 && j != 2) {
-                        if (j == 0 && (i == 1 || i == 2)) {
-                            if (this.board[i][j].getValue() != this.board[i][j + 1].getValue() && this.board[i][j].getValue() != this.board[i - 1][j].getValue() && this.board[i][j].getValue() != this.board[i + 1][j].getValue()) {
-                                ++count;
-                            }
-                        } else if (j == 3 && (i == 1 || i == 2)) {
-                            if (this.board[i][j].getValue() != this.board[i][j - 1].getValue() && this.board[i][j].getValue() != this.board[i - 1][j].getValue() && this.board[i][j].getValue() != this.board[i + 1][j].getValue()) {
-                                ++count;
-                            }
-                        } else if (this.board[i][j].getValue() != this.board[i][j - 1].getValue() && this.board[i][j].getValue() != this.board[i][j + 1].getValue() && this.board[i][j].getValue() != this.board[i - 1][j].getValue() && this.board[i][j].getValue() != this.board[i + 1][j].getValue()) {
-                            ++count;
+                    } else if (i == 3 && (j == 1 || j == 2)) {
+                        if (board[i][j].getValue() != board[i - 1][j].getValue()
+                                && board[i][j].getValue() != board[i][j + 1].getValue()
+                                && board[i][j].getValue() != board[i][j - 1].getValue()) {
+                            count++;
                         }
-                    } else if (this.board[i][j].getValue() != this.board[i - 1][j].getValue() && this.board[i][j].getValue() != this.board[i][j + 1].getValue() && this.board[i][j].getValue() != this.board[i][j - 1].getValue()) {
-                        ++count;
+                    } else if (j == 0 && (i == 1 || i == 2)) {
+                        if (board[i][j].getValue() != board[i][j + 1].getValue()
+                                && board[i][j].getValue() != board[i - 1][j].getValue()
+                                && board[i][j].getValue() != board[i + 1][j].getValue()) {
+                            count++;
+                        }
+                    } else if (j == 3 && (i == 1 || i == 2)) {
+                        if (board[i][j].getValue() != board[i][j - 1].getValue()
+                                && board[i][j].getValue() != board[i - 1][j].getValue()
+                                && board[i][j].getValue() != board[i + 1][j].getValue()) {
+                            count++;
+                        }
+                    } else {
+                        if (board[i][j].getValue() != board[i][j - 1].getValue()
+                                && board[i][j].getValue() != board[i][j + 1].getValue()
+                                && board[i][j].getValue() != board[i - 1][j].getValue()
+                                && board[i][j].getValue() != board[i + 1][j].getValue()) {
+                            count++;
+                        }
                     }
                 }
             }
         }
-
         if (count == 16) {
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 
     public void up() {
-        for (int i = 0; i < this.grids; ++i) {
+        for (int i = 0; i < this.grids; i++) {
             this.border = 0;
 
-            for (int j = 0; j < this.grids; ++j) {
-                if (this.board[j][i].getValue() != 0 && this.border <= j) {
-                    this.verticalMove(j, i, "up");
+            for (int j = 0; j < this.grids; j++) {
+                if (this.board[j][i].getValue() != 0) {
+                    if (this.border <= j) {
+                        this.verticalMove(j, i, "up");
+                    }
                 }
             }
         }
     }
 
     public void down() {
-        for (int i = 0; i < this.grids; ++i) {
+        for (int i = 0; i < this.grids; i++) {
             this.border = this.grids - 1;
 
             for (int j = this.grids - 1; j >= 0; --j) {
-                if (this.board[j][i].getValue() != 0 && this.border >= j) {
-                    this.verticalMove(j, i, "down");
+                if (this.board[j][i].getValue() != 0) {
+                    if (this.border >= j) {
+                        this.verticalMove(j, i, "down");
+                    }
                 }
             }
-        }
 
+        }
     }
 
     //move down
     private void verticalMove(int row, int col, String direction) {
         Tile initial = this.board[this.border][col];
         Tile compare = this.board[row][col];
-        if (initial.getValue() != 0 && initial.getValue() != compare.getValue()) {
+        if (initial.getValue() == 0 || initial.getValue() == compare.getValue()) {
+            if (row > border || (direction.equals("down") && (row < border))) {
+                int addScore = initial.getValue() + compare.getValue();
+                if (initial.getValue() != 0) {
+                    score += addScore;
+                }
+                initial.setValue(addScore);
+                compare.setValue(0);
+            }
+        } else {
             if (direction.equals("down")) {
-                --this.border;
+                border--;
             } else {
-                ++this.border;
+                border++;
             }
-            this.verticalMove(row, col, direction);
-        } else if (row > this.border || direction.equals("down") && row < this.border) {
-            int addScore = initial.getValue() + compare.getValue();
-            if (initial.getValue() != 0) {
-                this.score += addScore;
-            }
-            initial.setValue(addScore);
-            compare.setValue(0);
+            verticalMove(row, col, direction);
         }
     }
 
     //move left
 
     public void left() {
-        for (int i = 0; i < this.grids; ++i) {
+        for (int i = 0; i < this.grids; i++) {
             this.border = 0;
 
-            for (int j = 0; j < this.grids; ++j) {
-                if (this.board[i][j].getValue() != 0 && this.border <= j) {
-                    this.horizontalMove(i, j, "left");
+            for (int j = 0; j < this.grids; j++) {
+                if (this.board[i][j].getValue() != 0) {
+                    if (this.border <= j) {
+                        this.horizontalMove(i, j, "left");
+                    }
                 }
             }
         }
     }
 
     public void right() {
-        for (int i = 0; i < this.grids; ++i) {
-            this.border = this.grids - 1;
+        for (int i = 0; i < grids; ++i) {
+            border = grids - 1;
 
-            for (int j = this.grids - 1; j >= 0; --j) {
-                if (this.board[i][j].getValue() != 0 && this.border >= j) {
-                    this.horizontalMove(i, j, "right");
+            for (int j = grids - 1; j >= 0; --j) {
+                if (board[i][j].getValue() != 0) {
+                    if (border >= j) {
+                        horizontalMove(i, j, "right");
+                    }
                 }
             }
         }
     }
 
     private void horizontalMove(int row, int col, String direction) {
-        Tile initial = this.board[row][this.border];
-        Tile compare = this.board[row][col];
-        if (initial.getValue() != 0 && initial.getValue() != compare.getValue()) {
+        Tile initial = board[row][border];
+        Tile compare = board[row][col];
+        if (initial.getValue() == 0 || initial.getValue() == compare.getValue()) {
+            if (col > border || (direction.equals("right") && (col < border))) {
+                int addScore = initial.getValue() + compare.getValue();
+                if (initial.getValue() != 0) {
+                    score += addScore;
+                }
+                initial.setValue(addScore);
+                compare.setValue(0);
+            }
+        } else {
             if (direction.equals("right")) {
-                --this.border;
+                border--;
             } else {
-                ++this.border;
+                border++;
             }
-            this.horizontalMove(row, col, direction);
-        } else if (col > this.border || direction.equals("right") && col < this.border) {
-            int addScore = initial.getValue() + compare.getValue();
-            if (initial.getValue() != 0) {
-                this.score += addScore;
-            }
-            initial.setValue(addScore);
-            compare.setValue(0);
+            horizontalMove(row, col, direction);
         }
     }
+
 }
